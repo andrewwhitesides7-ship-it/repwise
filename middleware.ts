4304e17f459dd34b3e97d762e502b28306c1a86b
Andrew Whitesides <andrewwhitesides7@gmail.com>
2026-06-03 15:51:10 -0600

    fix middleware allow auth callback

--- middleware.ts
+++ middleware.ts
@@ -28,7 +28,7 @@ export async function middleware(request: NextRequest) {
 
     const { data: { user } } = await supabase.auth.getUser();
     const { pathname } = request.nextUrl;
-    const publicRoutes = ["/", "/login", "/signup"];
+    const publicRoutes = ["/", "/login", "/signup", "/auth/callback"];
     const isPublic = publicRoutes.includes(pathname);
 
     if (!user && !isPublic) {
